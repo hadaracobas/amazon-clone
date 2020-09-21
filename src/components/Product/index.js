e767dce1bb20a9df8b49eb07c52cc55063085ec5
hadaracobas <34766865+hadaracobas@users.noreply.github.com>
2020-09-21 14:52:48 +0200

    React context api and build add to basket button functionality

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./index.css";
+import { useStateValue } from "../../StateProvider";
 
 function Product({ id, title, price, rating, image }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // Add item to basket
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -18,9 +35,10 @@ function Product({ id, title, price, rating, image }) {
             ))}
         </div>
       </div>{" "}
-      {/* end .product__info */}
       <img src={image} />
-      <button type="button">Add to basket</button>
+      <button onClick={addToBasket} type="button">
+        Add to basket
+      </button>
     </div>
   );
 }
